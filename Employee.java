c978305212e81374b54c5b5c38f57c794d736e18
Venkatsai6301 <chelapakavenkatasai@gmail.com>
2022-11-16 16:46:12 +0530

    First Commit

---
+++ Employee.java
@@ -0,0 +1,13 @@
+record Employee(int Empid, String name, float salary, String designation)
+{
+Employee(){
+this( 12, "sai", 15000f, "Software");}
+
+public static void main(String []args){
+Employee a = new Employee( 12, "sai", 15000f, "Software");
+System.out.println(a);
+}
+}
+
+ 
+ 
